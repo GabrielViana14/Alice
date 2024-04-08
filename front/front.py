d45b6334e608ff182ee4b815a0d31791ae48e65c
GabrielViana14 <gabrielantonio1423@gmail.com>
2024-04-08 09:08:37 -0300

    front-end ok, testando criação de widgets em tempo real

--- front/front.py
+++ front/front.py
@@ -1,12 +1,17 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.utils import get_color_from_hex
 
 
 
+
+
 class MainApp(MDApp):
+    #Parametros alteraveis
+    bot_name = "Alice"
+
+
     def hex_to_color(self, hex_color):
         return get_color_from_hex(hex_color)
     
@@ -17,10 +22,21 @@ class MainApp(MDApp):
         Window.left = 100
         Window.always_on_top = True  # Mantém a janela sempre no topo
         self.title = "Assistente virtual" # Titulo da janela
-        Window.clearcolor = self.hex_to_color("#FD91A4")
+        self.clearcolor = self.hex_to_color("#FD91A4")
         return Builder.load_file("assets/kivy/main.kv")
     
+
+    def bot_response(self,text):
+        bot_resposta = self.root.ids.bot_response.copy()
+        bot_resposta.text = text
+        self.root.add_widget(bot_resposta)
+
+    def user_response(self,text):
+        user_resposta = self.root.ids.user_response.copy()
+        user_resposta.text = text
+        self.root.add_widget(user_resposta)
+    
     
 
-
-MainApp().run()
+if __name__ == "__main__":
+    MainApp().run()
